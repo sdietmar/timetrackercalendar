bc5ac8bdc1a142d3e5e4e5ebebfc4ed6b6b6d014
Dietmar <58474774+sdietmar@users.noreply.github.com>
2023-02-11 19:12:25 +0100

    create event with graph api Me: write python code that creates an event starting at the current system time and lasting 2 hours in a calendar using microsoft graph REST api ChatGPT: Here is a sample Python code that creates an event starting at the current system time and lasting 2 hours in a Microsoft Calendar using the Microsoft Graph REST API ChatGPT: Note that you'll need to replace "your_access_token" with a valid access token for the Microsoft Graph API. Also, make sure to install the requests and pytz libraries if you don't already have them installed

---
+++ create_event.py
@@ -0,0 +1,45 @@
+import requests
+import datetime
+import pytz
+
+# Microsoft Graph endpoint for creating an event
+endpoint = "https://graph.microsoft.com/v1.0/me/events"
+
+# Access token for authentication
+access_token = "your_access_token"
+
+# Get the current system time
+now = datetime.datetime.now(pytz.UTC)
+
+# Set the start and end time for the event
+start_time = now.isoformat()
+end_time = (now + datetime.timedelta(hours=2)).isoformat()
+
+# Event details
+event = {
+    "subject": "Sample event",
+    "start": {
+        "dateTime": start_time,
+        "timeZone": "UTC"
+    },
+    "end": {
+        "dateTime": end_time,
+        "timeZone": "UTC"
+    }
+}
+
+# Make a POST request to the Microsoft Graph API to create the event
+headers = {
+    "Authorization": "Bearer " + access_token,
+    "Content-Type": "application/json"
+}
+
+response = requests.post(endpoint, headers=headers, json=event)
+
+# Check the response status code
+if response.status_code != 201:
+    raise Exception("Failed to create event: " + response.text)
+
+# Get the newly created event from the response
+event = response.json()
+print("Event created successfully:", event)
