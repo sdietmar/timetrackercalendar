0036f39c919ec4f376e179906823c6aa5188402d
Dietmar <58474774+sdietmar@users.noreply.github.com>
2023-02-12 19:21:51 +0100

    implement azure authentication chatGPT got that part completely wrong. I update the code with an example found online: https://learndataanalysis.org/source-code-create-and-delete-outlook-calendar-events-using-microsoft-graph-api-in-python/

--- create_event.py
+++ create_event.py
@@ -1,14 +1,23 @@
 import requests
 import datetime
 import pytz
+from ms_graph import generate_access_token, GRAPH_API_ENDPOINT
 
 # Microsoft Graph endpoint for creating an event
 endpoint = "https://graph.microsoft.com/v1.0/me/events"
 
 # Access token for authentication
-# Load the access token from the text file
-with open("private_access_token.txt", "r") as file:
-    access_token = file.read().strip()
+with open("private_app_id.txt", "r") as file:
+    APP_ID = file.read().strip()
+
+SCOPES = ['Calendars.ReadWrite']
+
+# Generate Access Token
+access_token = generate_access_token(APP_ID, SCOPES)
+headers = {
+    'Authorization': 'Bearer ' + access_token['access_token']
+}
+
 
 # Get the current system time
 now = datetime.datetime.now(pytz.UTC)
